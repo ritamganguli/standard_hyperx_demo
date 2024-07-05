9002ae6bcfc05c06f5b013c5d27b4a7fced10c8e
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-07-05 16:18:56 +0530

    Update lambdatest2.py

--- lambdatest2.py
+++ lambdatest2.py
@@ -12,13 +12,10 @@ password = "Gmail@12345"
 build = os.getenv("LT_BUILD_NAME")
 
 # Define mobile emulation settings
-mobile_emulation = {
-    "deviceName": "iPhone X"  # You can change this to any device you want to emulate
-}
 
 options = ChromeOptions()
-options.browser_version = "latest"
-options.platform_name = "macOS Sonoma"
+options.set_capability("platformName", "macOS Sonoma")
+
 lt_options = {}
 lt_options["username"] = username
 lt_options["accessKey"] = access_key
@@ -30,14 +27,11 @@ lt_options["build"] = build
 lt_options["project"] = "unit_testing"
 lt_options["name"] = "basic_unit_selenium"
 lt_options["w3c"] = True
-lt_options["accessibility"] = True,
-lt_options["accessibility.wcagVersion"] = "wcag21a",
-lt_options["accessibility.needsReview"] = True,
+lt_options["accessibility"] = True
+lt_options["accessibility.wcagVersion"] = "wcag21a"
+lt_options["accessibility.needsReview"] = True
 lt_options["plugin"] = "python-python"
 options.set_capability("LT:Options", lt_options)
-
-# Add mobile emulation to ChromeOptions
-options.add_experimental_option("mobileEmulation", mobile_emulation)
 
 class FirstSampleTest(unittest.TestCase):
     driver = None
@@ -49,12 +43,13 @@ class FirstSampleTest(unittest.TestCase):
             ),
             options=options,
         )
+        # Set the viewport size to 375x812
+        self.driver.set_window_size(375, 812)
 
     def test_demo_site(self):
         driver = self.driver
         driver.implicitly_wait(10)
         driver.set_page_load_timeout(30)
-        driver.set_window_size(1920, 1080)
 
         print("Loading URL")
         driver.get("https://accounts.lambdatest.com/login")
@@ -113,45 +108,6 @@ class FirstSampleTest(unittest.TestCase):
         driver.find_element(By.XPATH, "//div[@class='osSelectorHeader flex']/div/span[2]").click()
         print("Selected iOS device")
         time.sleep(10)
-        print("Starting session over iOS real device")
-        driver.find_element(By.XPATH, "//span[text()='Start']/parent::span/parent::span/parent::button").click()
-        time.sleep(60)
-        driver.find_element(By.XPATH, "(//span[text()='End Session'])[1]").click()
-        print("Ending session")
-        driver.find_element(By.XPATH, "//span[text()='Yes, End Session']").click()
-        print("Ended the session")
-        time.sleep(30)
-        driver.find_element(By.XPATH, "//div[@class='aside__menu__link aside__menu__link__anchor']").click()
-        print("Going back to the main page")
-        time.sleep(15)
-        print("Going to automation")
-        driver.find_element(By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[4]").click()
-        time.sleep(25)
-        print("Currently inside web automation")
-        driver.find_element(By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[4]").click()
-        print("Now going to app automation")
-        driver.find_element(By.XPATH, "(//span[text()='App Automation'])[1]").click()
-        time.sleep(15)
-        driver.find_element(By.XPATH, "//div[@class='aside__menu__link aside__menu__link__anchor']").click()
-        print("Going back to the main page")
-        driver.find_element(By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[9]").click()
-        print("Going over more tools")
-        time.sleep(20)
-        driver.find_element(By.XPATH, "(//span[@class='aside__menu__bottom__submenu__item__icon'])[3]").click()
-        print("Going over integration")
-        time.sleep(10)
-        driver.find_element(By.XPATH, "//ul[@class='ltch-aside-menu-bottom-submenu-item-wrapper']/li[1]/a[1]").click()
-        time.sleep(30)
-        for i in range(600):
-            try:
-                element = driver.find_element(By.XPATH, "(//span[@class='aside__menu__bottom__submenu__item__icon'])[3]")
-                if element.is_displayed():
-                    print(f"Element is displayed in iteration {i}")
-                else:
-                    print(f"Element is not displayed in iteration {i}")
-            except:
-                print("exception")
-        print("Ending the test........")
 
     def tearDown(self):
         self.driver.quit()
