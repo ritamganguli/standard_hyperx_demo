758feb49c4783e184f8bf4a5dc37da03232a6534
ritamganguli <35348707+ritamganguli@users.noreply.github.com>
2024-07-24 14:50:01 +0530

    Update lambdatest2.py

--- lambdatest2.py
+++ lambdatest2.py
@@ -1,117 +1,145 @@
+import os
 import time
 import unittest
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-username = "shubhamr"
-access_key = "dl8Y8as59i1YyGZZUeLF897aCFvIDmaKkUU1e6RgBmlgMLIIhh"
-sign_in_mail = "shubhamr@lambdatest.com"
-password = "Gmail@12345"
-build = os.getenv("LT_BUILD_NAME")
-
-# Define mobile emulation settings
+username = os.getenv("LT_USERNAME")  # Replace the username
+access_key = os.getenv("LT_ACCESS_KEY")  # Replace the access key
+sign_in_mail=os.getenv("sign_in_mail")
+password=os.getenv("password")
+build=os.getenv("LT_BUILD_NAME")
 
 options = ChromeOptions()
-options.set_capability("platformName", "macOS Sonoma")
-
+options.browser_version = "latest"
+options.platform_name = "win10"
 lt_options = {}
 lt_options["username"] = username
 lt_options["accessKey"] = access_key
 lt_options["video"] = True
 lt_options["resolution"] = "1920x1080"
 lt_options["network"] = True
-lt_options["smartWait"] = 60
+lt_options["smartWait"]= 60
 lt_options["build"] = build
-lt_options["project"] = "unit_testing"
 lt_options["name"] = "basic_unit_selenium"
 lt_options["w3c"] = True
-lt_options["console"] = True
-lt_options["accessibility"] = True
-lt_options["lambdaMaskCommands"]=["setValues", "setCookies","getCookies"]
-lt_options["accessibility.wcagVersion"] = "wcag21a"
-lt_options["accessibility.needsReview"] = True
 lt_options["plugin"] = "python-python"
 options.set_capability("LT:Options", lt_options)
+
 
 class FirstSampleTest(unittest.TestCase):
     driver = None
 
     def setUp(self):
         self.driver = webdriver.Remote(
-            command_executor="http://{}:{}@hub.lambdatest.com/wd/hub".format(
-                username, access_key
-            ),
+            command_executor=f"http://{username}:{access_key}@hub.lambdatest.com/wd/hub",
             options=options,
         )
-        # Set the viewport size to 375x812
+        self.wait = WebDriverWait(self.driver, 60)
 
     def test_demo_site(self):
         driver = self.driver
-        driver.implicitly_wait(10)
         driver.set_page_load_timeout(30)
+        driver.set_window_size(1920, 1080)
 
+        # Url
         print("Loading URL")
         driver.get("https://accounts.lambdatest.com/login")
-        time.sleep(10)
-        # driver.set_window_size(375, 812)
 
-        time.sleep(15)
+        # Let's click on an element
         print("Entering Mail To Log in")
-        driver.add_cookie({"name": "example_cookie", "value": "cookie_value"})
-        driver.find_element(By.ID, "email").send_keys(sign_in_mail)
-        time.sleep(7)
+        self.wait.until(EC.presence_of_element_located((By.ID, "email"))).send_keys(sign_in_mail)
         print("Entering Password")
-        driver.find_element(By.ID, "password").send_keys(password)
-        time.sleep(7)
-        driver.find_element(By.ID, "login-button").click()
+        self.wait.until(EC.presence_of_element_located((By.ID, "password"))).send_keys(password)
+        self.wait.until(EC.element_to_be_clickable((By.ID, "login-button"))).click()
         print("Logging in")
-        time.sleep(35)
+
         print("Going over to a real-time session")
-        driver.find_element(By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[2]").click()
-        time.sleep(20)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[2]"))).click()
         print("Went into real device session")
+
         print("Selecting browser_version")
-        driver.find_element(By.XPATH, "//div[@id='version_126']").click()
-        time.sleep(10)
-        driver.find_element(By.XPATH, "//span[text()='Start']/parent::span/parent::button").click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='version_126']"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Start']/parent::span/parent::button"))).click()
         print("Starting a manual desktop session")
-        time.sleep(15)
-        driver.find_element(By.XPATH, "(//div[@class='aside__menu__link aside__menu__link__anchor_new'])[13]").click()
-        time.sleep(30)
+
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//div[@class='aside__menu__link aside__menu__link__anchor_new'])[13]"))).click()
         print("Closing the real-time desktop session")
-        driver.find_element(By.XPATH, "//span[text()='Yes, End Session']").click()
-        time.sleep(30)
-        driver.find_element(By.XPATH, "(//span[text()='Virtual Mobile'])[1]").click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Yes, End Session']"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//span[text()='Virtual Mobile'])[1]"))).click()
         print("Trying manual session over simulator/browser")
-        time.sleep(20)
-        driver.find_element(By.XPATH, "//input[@type='text']").click()
-        time.sleep(10)
-        driver.find_element(By.XPATH, "//span[text()='Start']/parent::span/parent::button").click()
-        print("Starting manual session of emulator......")
-        time.sleep(45)
-        driver.find_element(By.XPATH, "(//span[text()='End Session'])[1]").click()
-        print("Ending session")
-        driver.find_element(By.XPATH, "//span[text()='Yes, End Session']").click()
-        print("Exiting from the device")
-        driver.find_element(By.XPATH, "//div[@class='aside__menu__link aside__menu__link__anchor']").click()
-        print("Going back to the main page")
-        time.sleep(20)
-        print("Picking up real device")
-        driver.find_element(By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[3]").click()
+
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@type='text']"))).click()
+        time.sleep(5)
+        element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Start New Session']")))
+        driver.execute_script("arguments[0].click();", element)
         time.sleep(60)
-        print("Starting session over Android real device")
-        driver.find_element(By.XPATH, "//span[text()='Start']/parent::span/parent::span/parent::button").click()
-        time.sleep(30)
-        driver.find_element(By.XPATH, "(//span[text()='End Session'])[1]").click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//span[text()='End Session'])[1]"))).click()
         print("Ending session")
-        driver.find_element(By.XPATH, "//span[text()='Yes, End Session']").click()
-        print("Ended the session")
-        time.sleep(30)
-        driver.find_element(By.XPATH, "//div[@class='osSelectorHeader flex']/div/span[2]").click()
-        print("Selected iOS device")
+        time.sleep(7)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Yes, End Session']"))).click()
+        print("Exiting from the device")
+
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='aside__menu__link aside__menu__link__anchor']"))).click()
+        print("Going back to the main page")
+
+        print("Picking up real device")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[3]"))).click()
+        print("Starting session over Android real device")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='public-device-list']/ul/li[3]"))).click()
+        time.sleep(5)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Start']/parent::span/parent::span/parent::button"))).click()
+        time.sleep(50)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='endsession']"))).click()
+        print("Ending session")
+        time.sleep(7)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Yes, End Session']"))).click()
+
+        print("Selecting iOS device")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='osSelectorHeader flex']/div/span[2]"))).click()
+        print("Starting session over iOS real device")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='public-device-list']/ul/li[3]"))).click()
+        time.sleep(5)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Start']/parent::span/parent::span/parent::button"))).click()
+        time.sleep(50)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='endsession']"))).click()
+        print("Ending session")
+        time.sleep(7)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Yes, End Session']"))).click()
+
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='aside__menu__link aside__menu__link__anchor']"))).click()
+        print("Going back to the main page")
         time.sleep(10)
+        print("Going to automation")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[4]"))).click()
+        print("Currently inside web automation")
+        time.sleep(5)
+        print("Now going to app automation")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//span[text()='App Automation'])[1]"))).click()
+
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='aside__menu__link aside__menu__link__anchor']"))).click()
+        print("Going back to the main page")
+
+        print("Going over more tools")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='left_sidebar_header-items']/nav/ul/li[9]"))).click()
+
+        print("Going over integration")
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "(//span[@class='aside__menu__bottom__submenu__item__icon'])[3]"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//ul[@class='ltch-aside-menu-bottom-submenu-item-wrapper']/li[1]/a[1]"))).click()
+        for i in range(600):
+            try:
+                element = driver.find_element(By.XPATH, "(//span[@class='aside__menu__bottom__submenu__item__icon'])[3]")
+                if element.is_displayed():
+                    print(f"Element is displayed in iteration {i}")
+                else:
+                    print(f"Element is not displayed in iteration {i}")
+            except: 
+                print("exception")
+        time.sleep(15)
+        print("Ending the test........")
 
     def tearDown(self):
         self.driver.quit()
